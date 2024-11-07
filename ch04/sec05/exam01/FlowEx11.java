e6857ef979ed0a65b0fb64caa89afad60d106c88
xdo13 <xdo13@naver.com>
2024-11-07 16:58:11 +0900

    case문 2가지 종류

---
+++ ch04/sec05/exam01/FlowEx11.java
@@ -0,0 +1,36 @@
+package ch04.sec05.exam01;
+
+import java.util.Scanner;
+
+public class FlowEx11 {
+
+	public static void main(String[] args) {
+		System.out.println("당신의 주민번호를 입력하세요");
+		
+		Scanner scanner = new Scanner(System.in);
+		String regNo = scanner.nextLine();
+		char gender = regNo.charAt(7); //입력받은 번호와 8번째 문자를 gender에 저장
+		switch(gender) {
+		case'1' : case'3':
+			switch(gender) {
+			case'1':
+				System.out.println("당신은 2000년 이전에 출생한 남자입니다");
+				break;
+			case'3':
+				System.out.println("당신은 2000년 이후에 출생한 남자입니다");
+			}
+			break;
+		case'2': case '4':
+			switch(gender) {
+			case'2':
+				System.out.println("당신은 2000년 이전에 출생한 여자입니다");
+				break;
+			case '4':
+				System.out.println("당신은 2000년 이후에 출생한 여자입니다.");
+				break;
+			}
+		}
+
+	}
+
+}
